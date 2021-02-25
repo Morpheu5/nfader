da35036965a702d4e0327f4942ee7cba03380dd7
Andrea Franceschini <andrea.franceschini@gmail.com>
2021-02-25 22:30:48 +0000

    There's some semblance of working

--- nfader.c
+++ nfader.c
@@ -6,25 +6,23 @@ static t_class *nfader_tilde_class;
 
 typedef struct _nfader_tilde {
     t_object x_obj;
+    t_float x_f;
     t_int n;
     t_float t;
-    t_atom *in_vec;
+    t_inlet *in_t;
+    t_inlet **in_vec;
     t_outlet *out;
 } t_nfader_tilde;
 
-void nfader_setN(t_nfader_tilde *x, t_floatarg n) {
-    x->n = n < 2 ? 2 : n;
-}
-
 void *nfader_new(t_floatarg n) {
     t_nfader_tilde *x = (t_nfader_tilde *)pd_new(nfader_tilde_class);
-    t_atom *vec;
 
-    nfader_setN(x, n);
+    x->n = (t_int)floor(n < 2 ? 2 : n);
     x->t = 0.0f;
-    vec = x->in_vec = (t_atom *)getbytes(n * sizeof(*x->in_vec));
-    for (int i = 0; i < n; ++i) {
-        signalinlet_new(&x->x_obj, 0.0f);
+    x->in_t = floatinlet_new(&x->x_obj, &x->t);
+    x->in_vec = (t_inlet **)getbytes(n * sizeof(t_inlet *));
+    for (int i = 0; i < x->n; ++i) {
+        x->in_vec[i] = signalinlet_new(&x->x_obj, 0.0f);
     }
     x->out = outlet_new(&x->x_obj, &s_signal);
 
@@ -32,7 +30,26 @@ void *nfader_new(t_floatarg n) {
 }
 
 void nfader_free(t_nfader_tilde *x) {
+    for (int i = 0; i < x->n; ++i) {
+        inlet_free(x->in_vec[i]);
+    }
+    freebytes(x->in_vec, x->n * sizeof(t_inlet *));
+    inlet_free(x->in_t);
     outlet_free(x->out);
+}
+
+t_int *nfader_tilde_perform(t_int *w) {
+    t_nfader_tilde *x = (t_nfader_tilde *)w[1];
+    t_inlet **in_vec = (t_inlet **)w[2];
+    t_sample *out = (t_sample *)w[3];
+
+    post("t = %f\nn = %d\n", x->t, x->n);
+
+    return w+5;
+}
+
+void nfader_tilde_dsp(t_nfader_tilde *x, t_signal **sp) {
+    dsp_add(nfader_tilde_perform, 4, x, sp, sp[x->n]->s_vec, (t_int)sp[0]->s_n);
 }
 
 void nfader_tilde_setup(void) {
@@ -40,7 +57,11 @@ void nfader_tilde_setup(void) {
                              (t_newmethod)nfader_new,
                              (t_method)nfader_free,
                              sizeof(t_nfader_tilde),
-                             CLASS_DEFAULT,
+                             CLASS_PATCHABLE | CLASS_NOINLET,
                              A_DEFFLOAT,
                              0);
+    CLASS_MAINSIGNALIN(nfader_tilde_class, t_nfader_tilde, x_f);
+    class_addmethod(nfader_tilde_class,
+                    (t_method)nfader_tilde_dsp,
+                    gensym("dsp"), A_CANT, 0);
 }
