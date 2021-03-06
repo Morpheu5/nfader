b9db52d267b8a9ba64514c32071042c5d1457feb
Andrea Franceschini <andrea.franceschini@gmail.com>
2021-03-06 17:41:53 +0000

    Some tests going wrong

--- nfader.c
+++ nfader.c
@@ -10,7 +10,7 @@ typedef struct _nfader_tilde {
     t_int n;
     t_float t;
     t_inlet *in_t;
-    t_inlet **in_vec;
+    t_signal **in_vec;
     t_outlet *out;
 } t_nfader_tilde;
 
@@ -19,19 +19,19 @@ void *nfader_new(t_floatarg n) {
 
     x->n = (t_int)floor(n < 2 ? 2 : n);
     x->t = 0.0f;
-    x->in_t = floatinlet_new(&x->x_obj, &x->t);
-    x->in_vec = (t_inlet **)getbytes(n * sizeof(t_inlet *));
+    x->in_t = floatinlet_new((t_object *)x, &x->t);
+    x->in_vec = (t_signal **)getbytes(n * sizeof(t_signal *));
     for (int i = 0; i < x->n; ++i) {
-        x->in_vec[i] = signalinlet_new(&x->x_obj, 0.0f);
+        x->in_vec[i] = (t_signal *)signalinlet_new((t_object *)x, 0.0f); // (&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
     }
-    x->out = outlet_new(&x->x_obj, &s_signal);
+    x->out = outlet_new((t_object *)x, &s_signal);
 
     return (void *)x;
 }
 
 void nfader_free(t_nfader_tilde *x) {
     for (int i = 0; i < x->n; ++i) {
-        inlet_free(x->in_vec[i]);
+        inlet_free((t_inlet *)x->in_vec[i]);
     }
     freebytes(x->in_vec, x->n * sizeof(t_inlet *));
     inlet_free(x->in_t);
@@ -40,10 +40,16 @@ void nfader_free(t_nfader_tilde *x) {
 
 t_int *nfader_tilde_perform(t_int *w) {
     t_nfader_tilde *x = (t_nfader_tilde *)w[1];
-    t_inlet **in_vec = (t_inlet **)w[2];
+    t_signal **in_vec = (t_signal **)w[2];
     t_sample *out = (t_sample *)w[3];
+    int bs = (int)w[4];
 
-    post("t = %f\nn = %d\n", x->t, x->n);
+    t_signal *in1 = in_vec[0];
+    t_sample *in = in1->s_vec;
+
+    for (int i = 0; i < bs; ++i) {
+        out[i] = x->t;// in[i];
+    }
 
     return w+5;
 }
